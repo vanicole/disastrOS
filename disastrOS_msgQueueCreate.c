84cc1d5e8fe6a00fd594ebb3ab93d42bc8b47a13
Vanina Nicole Muscas <muscas.1655834@studenti.uniroma1.it>
2021-02-22 17:36:59 +0100

    Inserite syscalls per gestire msg queue

---
+++ disastrOS_msgQueueCreate.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "disastrOS.h"
+#include "disastrOS_syscalls.h"
+#include "disastrOS_msg_queue.h"
+#include "disastrOS_descriptor.h"
+
+
+void internal_msgQueueCreate() {
+
+}
---
+++ disastrOS_msgQueueWrite.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "disastrOS.h"
+#include "disastrOS_syscalls.h"
+#include "disastrOS_msg_queue.h"
+#include "disastrOS_descriptor.h"
+
+void internal_msgQueueWrite() {
+
+
+}
