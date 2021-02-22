0d98edd0c9ece659488ad97c203bd9d1eb5dd4e8
vanicole <muscas.1655834@studenti.uniroma1.it>
2021-02-22 17:21:42 +0100

    Caricamento resource e funzioni annesse

---
+++ disastrOS_resource.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "linked_list.h"
+#include "disastrOS_constants.h"
+#include "disastrOS_pcb.h"
+
+#define DSOS_GENERIC_RESOURCE 0
+#define DSOS_MSG_QUEUE_RESOURCE 1
+
+typedef struct Resource {
+  ListItem list;
+  const char *name;
+  int rid;
+  int type;
+  ListHead descriptors_ptrs;
+} Resource;
+
+void Resource_init();
+
+Resource* Resource_alloc(const char *name, int rid, int type);
+int Resource_free(Resource* resource);
+
+typedef ListHead ResourceList;
+
+Resource* ResourceList_byId(ResourceList* l, int id);
+
+void ResourceList_print(ListHead* l);
